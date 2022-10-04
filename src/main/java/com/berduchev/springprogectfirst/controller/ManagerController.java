064f1b1e3b81fc62176cca815e95cacdf17a1502
Санёк Виничук <sashavinichuk@gmail.com>
2022-10-04 12:08:55 +0300

    ManagerController

--- src/main/java/com/berduchev/springprogectfirst/controller/ManagerController.java
+++ src/main/java/com/berduchev/springprogectfirst/controller/ManagerController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -38,4 +39,31 @@ public class ManagerController {
         return null;
     }
 
+    @GetMapping("/create")
+    public String create(){
+        return "/managers/create";
+    }
+
+    @PostMapping("/createmanager")
+    public String createmanager(@RequestParam(name="firstname") String firstname,
+                                @RequestParam(name = "lastname")String lastname,
+                                @RequestParam(name = "passportid") String passportid,
+                                @RequestParam(name = "expiriance") String expiriance,
+                                @RequestParam(name = "sales")String sales, Model model){
+        System.out.println(this);
+        System.out.println(firstname);
+        System.out.println(lastname);
+        System.out.println(passportid);
+        Manager manager = new Manager();
+        manager.setFirstName(firstname);
+        manager.setLastName(lastname);
+        manager.setExpiriance(Integer.parseInt(expiriance));
+        manager.setPassportid(passportid);
+        manager.setSales(Integer.parseInt(sales));
+        managerService.createManager(manager);
+        List<Manager> managers = managerService.getAllManager();
+        model.addAttribute("managers", managers);
+        System.out.println(managers);
+        return "/managers/managers";
+    }
 }
